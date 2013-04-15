e2155eb51c1d9d13316a219267880e19d1a78051
MurkyMittens <murkymittens@mailmetrash.com>
2013-04-14 23:59:30 -0400

    initial commit

---
+++ Minecraft.class.php
@@ -0,0 +1,87 @@
+﻿<?php
+
+class Minecraft {
+	private $db;
+	
+	public function __construct(&$database) {
+		$this->db = $database;
+	}
+	
+	public function authenticateUserPassword($username, $password) {
+		$query = $this->db->prepare("SELECT `password` FROM `users` WHERE `username`=:username LIMIT 1");
+		$query->bindValue(":username", $username, PDO::PARAM_STR);
+		$query->execute();
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		if($result === FALSE) {
+			return FALSE; // Username does not exist
+		}
+		if(sha1($password) == $result['password']) {
+			return TRUE;
+		} else {
+			return FALSE: // Passwords don't match
+		}
+	}
+	
+	public function authenticateUserSession($username, $sessionId) {
+		$query = $this->db->prepare("SELECT `id` FROM `sessions` WHERE `username`=:username ORDER BY `time` DESC LIMIT 1");
+		$query->bindValue(":username", $username, PDO::PARAM_STR);
+		$query->execute();
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		if($result === FALSE) {
+			return FALSE; // User hasn't logged in
+		}
+		if($sessionId == $result['id']) {
+			return TRUE;
+		} else {
+			return FALSE: // Someone's trying to session hijack; or my script screwed up
+		}
+	}
+	
+	public function generateUserId($username) {
+		return md5($username);
+	}
+	
+	public function generateSessionId($username) {
+		$sessionId = sha1($username . time());
+		$stmt = $this->db->prepare("INSERT INTO `sessions` (`id`, `username`) VALUES (:id, :username)");
+		$stmt->bindValue(":id", $sessionId, PDO::PARAM_STR);
+		$stmt->bindValue(":username", $username, PDO::PARAM_STR);
+		$stmt->execute();
+		return $sessionId;
+	}
+	
+	public function getGameVersion() {
+		return time() * 1000;
+	}
+	
+	public function generateServerJoinRequest($username, $sessionId, $serverId) {
+		if(!$this->authenticateUserSession($username, $sessionId)) {
+			return FALSE;
+		}
+		$stmt = $this->db->prepare("INSERT INTO `server_joins` (`username`, `server_id`) VALUES (:username, :server_id)");
+		$stmt->bindValue(":username", $username, PDO::PARAM_STR);
+		$stmt->bindValue(":server_id", $serverId, PDO::PARAM_STR);
+		$stmt->execute();
+		return TRUE;
+	}
+	
+	public function verifyServerJoinRequest($username, $serverId) {
+		$query = $this->db->prepare("SELECT `serverId` FROM `server_joins` WHERE `username`=:username LIMIT 1");
+		$query->bindValue(":username", $username, PDO::PARAM_STR);
+		$query->execute();
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		if($result === FALSE) {
+			return FALSE; // User has not initiated server join
+		}
+		if($serverId == $result['server_id']) {
+			$query = $this->db->prepare("DELETE FROM `server_joins` WHERE `username`=:username");
+			$query->bindValue(":username", $username, PDO::PARAM_STR);
+			$query->execute();
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,76 @@
+﻿<?php
+
+// Launcher authentication
+// REQUEST[POST]: https://login.minecraft.net/?user=<username>&password=<password>&version=<launcher version>
+// RESPONSE: 1343825972000:deprecated:SirCmpwn:7ae9007b9909de05ea58e94199a33b30c310c69c:dba0c48e1c584963b9e93a038a66bb98
+// $client_login_response = "{GAME_VERSION}:deprecated:{USER_NAME}:{SESSION_ID}:{USER_ID}";
+
+// Client keep-alive
+// REQUEST[GET]: https://login.minecraft.net/session?name=<username>&session=<session id>
+// RESPONSE: any; discarded by client
+
+// Join server
+// REQUEST[GET]: http://session.minecraft.net/game/joinserver.jsp?user=<username>&sessionId=<session id>&serverId=<server hash>
+// RESPONSE: OK
+
+// Server username authentication
+// REQUEST[GET]: http://session.minecraft.net/game/checkserver.jsp?user=<username>&serverId=<server hash>
+// RESPONSE: YES
+
+include("Minecraft.class.php");
+
+$database = new PDO("mysql:host=localhost;dbname=minecraft;charset=utf8", "root", "");
+$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+$minecraft = new Minecraft($database);
+
+function varex($variable_name) {
+	if(isset($_POST[$variable_name])) {
+		return $_POST[$variable_name];
+	} elseif(isset($_GET[$variable_name])) {
+		return $_GET[$variable_name];
+	} else {
+		return null;	
+	}
+}
+
+if(isset($_GET['action'])) {
+	$action = $_GET['action'];
+	switch($action) {
+		case "login":
+			$username = varex('user');
+			$password = varex('password');
+			if($minecraft->authenticateUser($username, $password)) {
+				echo $minecraft->getGameVersion() . ":deprecated:{$username}:" . $minecraft->generateSessionId($username) . 
+					":" . $minecraft->generateUserId($username);
+			} else {
+				echo "Bad login";	
+			}
+		break;
+		case "keepalive":
+			echo "KK BRO";
+		break;
+		case "joinserver":
+			$username = varex('user');
+			$session_id = varex('sessionId');
+			$server_id = varex('serverId');
+			if($minecraft->generateServerJoinRequest($username, $session_id, $server_id)) {
+				echo "OK";
+			} else {
+				echo "Not cool bro";	
+			}
+		break;
+		case "checkserver":
+			$username = varex('user');
+			$server_id = varex('serverId');
+			if($minecraft->verifyServerJoinRequest($username, $server_id)) {
+				echo "YES";
+			} else {
+				echo "Not cool bro";	
+			}
+		break;
+	}	
+}
+
+?>
