a0b73149873340e7a6f8f88aa84da79fe6a0fe7f
MurkyMittens <murkymittens@mailmetrash.com>
2013-04-15 00:48:15 -0400

    added database schema

---
+++ database.sql
@@ -0,0 +1,54 @@
+﻿-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.27 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2013-04-15 00:47:39
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+-- Dumping structure for table minecraft.server_joins
+DROP TABLE IF EXISTS `server_joins`;
+CREATE TABLE IF NOT EXISTS `server_joins` (
+  `username` varchar(16) DEFAULT NULL,
+  `server_id` varchar(40) DEFAULT NULL,
+  KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table minecraft.server_joins: ~0 rows (approximately)
+/*!40000 ALTER TABLE `server_joins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `server_joins` ENABLE KEYS */;
+
+
+-- Dumping structure for table minecraft.sessions
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(40) DEFAULT NULL,
+  `username` varchar(16) DEFAULT NULL,
+  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table minecraft.sessions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+
+
+-- Dumping structure for table minecraft.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(16) NOT NULL DEFAULT '',
+  `password` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table minecraft.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`username`, `password`) VALUES
+	('TestUser1', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
